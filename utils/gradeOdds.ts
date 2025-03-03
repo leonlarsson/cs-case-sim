44fe41fe4eff728c728608774a472026aa976a0e
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2025-03-03 16:13:43 +0000

    adjust item drop rates and add missing classified drops from souvenir cases

--- utils/gradeOdds.ts
+++ utils/gradeOdds.ts
@@ -4,19 +4,22 @@ type GradeOddsType = {
   [grade in ItemGrade]: number;
 };
 
+// Define drop rates - taken from https://www.csgo.com.cn/news/gamebroad/20170911/206155.shtml
 export const gradeOddsCase = {
-  // "Rare Special Item": 1.0026,
-  "Mil-Spec Grade": 0.7992,
-  Restricted: 0.1598,
-  Classified: 0.032,
-  Covert: 0.0064,
-  "Rare Special Item": 0.0026,
+  // "Rare Special Item": 1,
+  "Mil-Spec Grade": 0.79923,
+  Restricted: 0.15985,
+  Classified: 0.03197,
+  Covert: 0.00639,
+  "Rare Special Item": 0.00256,
 } as GradeOddsType;
 
 export const gradeOddsSouvenir = {
-  "Consumer Grade": 0.7992,
-  "Industrial Grade": 0.1598,
-  "Mil-Spec Grade": 0.032,
-  Restricted: 0.0064,
-  Covert: 0.0014,
+  // "Covert" : 1,
+  "Consumer Grade": 0.79872,
+  "Industrial Grade": 0.15974,
+  "Mil-Spec Grade": 0.03328,
+  Restricted: 0.00666,
+  Classified: 0.00133,
+  Covert: 0.00027,
 } as GradeOddsType;
