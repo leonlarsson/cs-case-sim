e55c69668b6e4cbe8780d978394461edb2e75e25
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2025-04-02 14:44:20 +0000

    disable maintenance mode

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const maintenanceMode = true;
+const maintenanceMode = false;
 
 export const middleware = (request: NextRequest) => {
   // Maintenance mode
